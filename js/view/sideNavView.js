ec4d277c00ed56e124417ea30b61dd101f345f57
mollyz <zyzhang21@gmail.com>
2019-04-15 10:48:12 -0600

    back to Lab3 version for presentation

--- js/view/sideNavView.js
+++ js/view/sideNavView.js
@@ -25,7 +25,6 @@ var SideNavView = function (container, model) {
 			var html="";
 			html+="<div class='row' id='Heading'><div class='col-sm-7'>Name:</div><div class='col-sm-4'>Cost:</div></div>";
 
-
 	 		
 	 		//得出菜单
 			var menuDish= model.getFullMenu();
@@ -37,9 +36,8 @@ var SideNavView = function (container, model) {
 			  	
 			  	var priceForAllPeople=priceForSingle*model.getNumberOfGuests();
 			  	html+="<div class='row'><div class='col-sm-7'>"+item.name+"</div><div class='col-sm-3'>"+priceForAllPeople +"</div><div class='col-sm-1'><img id='"+item.id+"'src='remove.png'></div></div>";
-
+		
 			});
-
 
 			var pendingID=model.getPendingID();
 			html+="<div class='row'><div class='col-sm-7'>Pending</div>";
@@ -68,16 +66,13 @@ var SideNavView = function (container, model) {
 
 
 
-
 	this.update= function(obj){
 		//改变人数
  		this.numberOfGuests.html(model.getNumberOfGuests());
  		if(obj=="num"||obj=="dishChange" ||obj=="pendingChange"){
 			//输出标题
 			var html="";
-
 			html+="<div class='row' id='Heading'><div class='col-sm-7'>Name:</div><div class='col-sm-4'>Cost:</div></div>";
-
 
 	 		
 	 		//得出菜单
@@ -89,13 +84,11 @@ var SideNavView = function (container, model) {
 			  	var priceForSingle= model.getPriceForDish(item.id);
 			  	
 			  	var priceForAllPeople=priceForSingle*model.getNumberOfGuests();
-
 			  	html+="<div class='row'><div class='col-sm-7'>"+item.name+"</div><div class='col-sm-3'>"+priceForAllPeople +"</div><div class='col-sm-1'><img id='"+item.id+"'src='remove.png'></div></div>";
-
+		
 			});
 
 			var pendingID=model.getPendingID();
-
 			html+="<div class='row'><div class='col-sm-7'>Pending</div>";
 
 			if(pendingID!=0){
@@ -103,22 +96,19 @@ var SideNavView = function (container, model) {
 				var priceForPending= model.getPriceForDish(pendingID);
 				var priceForPendingAll=priceForPending*model.getNumberOfGuests();
 			
-
 				html+="<div class='col-sm-3'>"+priceForPendingAll+"</div><div class='col-sm-1'></div></div>";
 				var totalPriceForMenu=model.getTotalMenuPrice()+priceForPendingAll;
 				
 			}else{
 				html+="<div class='col-sm-3'>0</div><div class='col-sm-1'></div></div>";
-alPriceForMenu=model.getTotalMenuPrice();
+				var totalPriceForMenu=model.getTotalMenuPrice();
 				}
 			
 
 			
 		
 			//输出总价 
-
 			html+="<div class='row'><div class='col-sm-7'>"+"Total:"+"</div><div class='col-sm-4'> "+totalPriceForMenu+" SEK</div></div>";
-
 			
 			this.PriceTable.html(html);
 
